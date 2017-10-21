5e8708dc43eef9fe7d55a9e6334180896485cba2
touir1 <touir.mat@gmail.com>
2017-10-21 17:30:10 +0100

    added consult unread messages: (problem with client.fetchUnread)

--- principalScreen.py
+++ principalScreen.py
@@ -1,21 +1,25 @@
 from utils import *
 import messageListScreen
 import sendMessageScreen
+import unreadMessageScreen
 
 
 def openScreen(client, session):
     choice = 0
-    while choice<1 or choice>3:
+    while choice<1 or choice>4:
         console_clear()
         print('choose a number:')
         print('1- open message list')
-        print('2- send a message')
-        print('3- exit')
+        print('2- open unread messages')
+        print('3- send a message')
+        print('4- exit')
         choice = to_int(input('\nchoice: '))
 
-    if choice == 3:
+    if choice == 4:
         quit()
     elif choice ==1:
         messageListScreen.openScreen(client, session)
     elif choice ==2:
+        unreadMessageScreen.openScreen(client, session)
+    elif choice ==3:
         sendMessageScreen.openScreen(client=client, session=session)
---
+++ unreadMessageScreen.py
@@ -0,0 +1,55 @@
+from utils import *
+import principalScreen
+import sendMessageScreen
+
+
+def openScreen(client, session):
+
+    users = {}
+    offset = 0
+    jump = 10
+
+    for u in client.fetchAllUsers():
+        users[u.uid] = u.name
+    users[client.uid] = client.fetchUserInfo(client.uid)[client.uid].name
+
+    while True:
+        console_clear()
+        print('list of unread messages:')
+        #buffer = Buffer()
+        threads = client.fetchUnread()
+        for idx, thread in enumerate(threads):
+            messages = client.fetchThreadMessages(thread_id= thread.uid, limit=1)
+            #buffer.push('------------------------------------------\n')
+            print('------------------------------------------')
+            #buffer.push(idx+1,'-',thread.name,'(last: ',users[messages[0].author],'): ',toUTF8(messages[0].text)+'\n')
+            print(idx+1,'-',thread.name,'(last: ',users[messages[0].author],'): ',toUTF8(messages[0].text))
+
+        print('------------------------------------------')
+
+        options = ['b','e']
+
+        if offset !=0:
+            print('p - previous')
+            options.append('p')
+        if len(threads) == jump:
+            print('n - next')
+            options.append('n')
+        print('b - back to main menu')
+        print('e - exit')
+
+        choice = input('choice: ')
+
+        if choice in options:
+            if choice == 'e':
+                break
+            elif choice == 'b':
+                principalScreen.openScreen(client, session)
+            elif choice =='n':
+                offset += jump
+            else:
+                offset -= jump
+        elif isInt(choice) and (1 <= to_int(choice) <= jump):
+            sendMessageScreen.openScreen(client,session,threads[to_int(choice)-1])
+            break
+
